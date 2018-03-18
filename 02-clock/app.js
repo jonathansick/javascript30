df9db6567984a10103c5a8c0a2501d7529761c1a
Jonathan Sick <jsick@lsst.org>
2018-03-17 17:51:20 -0700

    Rework clock so angle increases monotonically
    This prevents the second hand whiplash as it goes from "360" to "1"
    degree. It's a bit of a hack, and has a couple edge cases that would
    need to be solved.

--- 02-clock/app.js
+++ 02-clock/app.js
@@ -2,15 +2,29 @@ const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
-function tickClock() {
-  const now = new Date();
-  const seconds = now.getSeconds();
-  const minutes = now.getMinutes();
-  const hours = now.getHours();
+const initialDate = new Date();
+var secondTally = initialDate.getSeconds();
+var minuteTally = initialDate.getMinutes();
+var hourTally = initialDate.getHours();
 
-  const secondsDegrees = (seconds / 60) * 360 + 90;
-  const minutesDegrees = (minutes / 60) * 360 + 90;
-  const hoursDegrees = (hours / 60) * 360 + 90;
+function tickClock() {
+  // Always monotonically increment s/m/h so that the angle increases
+  // monotoically. This way the css transitions are continuous.
+  // The edge case is if the clock is loaded exactly at the top of the hour,
+  // then the clock would be ahead :(
+  secondTally += 1;
+  const now = new Date();
+  if (now.getSeconds() === 0) {
+    minuteTally += 1;
+  }
+  if (now.getMinutes() === 0 && now.getSeconds === 0) {
+    hourTally += 1;
+    console.log('hour increment');
+  }
+
+  const secondsDegrees = (secondTally / 60) * 360 + 90;
+  const minutesDegrees = (minuteTally / 60) * 360 + 90;
+  const hoursDegrees = (hourTally / 12) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
